197b2d6a6f8e4fa7048254611aa1b4334f766652
shomrash <shomrash@imac-shomrash.local>
2018-12-20 19:04:14 +0300

    Initial Commit

---
+++ Divan/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Divan
+//
+//  Created by shomrash on 20.12.2018.
+//  Copyright © 2018 shomrashDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
